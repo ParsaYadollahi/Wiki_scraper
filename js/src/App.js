76a901c950b072da53010a35152d7d10dbe0bac7
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-11-10 17:44:11 -0500

    graph base added

--- js/src/App.js
+++ js/src/App.js
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as d3 from "d3";
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 import Topbar from './topbar.js'
+import Tree from 'react-tree-graph'
 
 class App extends React.Component {
+  
   render() {
+    const data = {
+      "name": "Eve",
+      "children": [
+        {
+          "name": "Cain"
+        },
+        {
+          "name": "Seth",
+          "children": [
+            {
+              "name": "Enos"
+            },
+            {
+              "name": "Noam"
+            }
+          ]
+        },
+        {
+          "name": "Abel"
+        },
+        {
+          "name": "Awan",
+          "children": [
+            {
+              "name": "Enoch"
+            }, {
+              "name": "yeeew"
+            }, {
+              "name": "NIKOLAI"
+            }, {
+              "name": "Parsa"
+            }, {
+              "name": "HELLO"
+            }
+          ]
+        },
+        {
+          "name": "Azura"
+        }
+      ]
+    }
+    
       return (
           <div>
           <Topbar />
+          <Tree
+            data={data}
+            height={200}
+            width={400}/>
           </div>
       );
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App;

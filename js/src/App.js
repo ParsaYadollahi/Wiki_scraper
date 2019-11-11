192a36a6ca3c2545f9da4ffa5379aaf965bb785d
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-11-10 22:17:34 -0500

    css added to graph and zoom option

--- js/src/App.js
+++ js/src/App.js
@@ -5,9 +5,22 @@ import logo from './logo.svg';
 import './App.css';
 import Topbar from './topbar.js'
 import Tree from 'react-tree-graph'
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 class App extends React.Component {
   
+  render() {
+      return (
+          <div>
+          <Topbar />
+          </div>
+      );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+class Graph extends React.Component {
   render() {
     const data = {
       "name": "Eve",
@@ -44,25 +57,42 @@ class App extends React.Component {
               "name": "HELLO"
             }
           ]
-        },
-        {
-          "name": "Azura"
         }
       ]
     }
-    
-      return (
+    return (
+          <TransformWrapper
+          defaultScale={1}
+          maxScale={8}
+          defaultPositionX={100}
+          defaultPositionY={100}
+          >
+          {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+          <React.Fragment>
+          <div className="tools">
+              <button onClick={zoomIn}>Zoom in</button>
+              <button onClick={zoomOut}>Zoom out</button>
+            </div>
+            <TransformComponent>
           <div>
-          <Topbar />
           <Tree
             data={data}
-            height={200}
-            width={400}/>
+            duration = {500}
+            nodeRadius={15}
+            margins={{ top: 20, bottom: 10, left: 30, right: 200 }}
+            height={600}
+            width={600}
+            animated
+            duration={800}/>
           </div>
+          </TransformComponent>
+          </React.Fragment>
+        )}
+      </TransformWrapper>
       );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Graph />, document.getElementById('graph'));
 
 export default App;

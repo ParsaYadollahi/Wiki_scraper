f72cd8277925ffac589519d94abe28b74aa84c2d
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-11-14 17:15:34 -0500

    limited content to 2 sentences

--- wiki_scrapper.py
+++ wiki_scrapper.py
@@ -15,19 +15,28 @@ links = soup.select("p a[href*=wiki]") # only part of html wanted
 # Json info going to input into db
 json_db = {}
 json_db.setdefault('name', query)
-# json_db.setdefault('content', wikipedia.summary(query))
+content = wikipedia.summary(query).split('.')
+desc = ''
+for c in range(2):
+    desc += content[c] + '.'
+
+json_db.setdefault('content', desc)
 json_db.setdefault('url', wikipedia.page(query).url)
 children = []
 
 # add children to root
-for i in range(3):
+for i in range(2):
     child_dict = {}
     url = urllib.parse.urljoin(base, links[i]['href'])
     title = url.split('/')[-1]
-    # content = wikipedia.summary(title)
+    content = wikipedia.summary(title)
+    content = content.split('.')
+    decr = ''
+    for c in range(3):
+        decr += content[c] + '.'
 
     child_dict.setdefault("name", title)
-    # child_dict.setdefault("content", content)
+    child_dict.setdefault("content", decr)
     child_dict.setdefault("url", url)
     child_dict.setdefault('children', [])
     children.append(child_dict)

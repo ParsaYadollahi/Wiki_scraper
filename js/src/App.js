c9e9183885b2b56ca855a7fae6d466751ea0be0b
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-11-15 21:44:37 -0500

    added block on multiple presses on nodes

--- js/src/App.js
+++ js/src/App.js
@@ -3,17 +3,6 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import Topbar from './topbar.js'
 import G from './Tree.js'
-import Tree from 'react-tree-graph'
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import axios from 'axios'
-import { Popup } from 'semantic-ui-react'
-
-// Import the icons
-import { Icon } from 'react-icons-kit'
-import {zoom_in} from 'react-icons-kit/ikons/zoom_in'
-import {zoom_out} from 'react-icons-kit/ikons/zoom_out'
-import {link} from 'react-icons-kit/icomoon/link'
-import {info} from 'react-icons-kit/icomoon/info'
 
 class App extends React.Component {
   
